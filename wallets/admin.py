36a741ddaced73367bf36b22c7ae068a7726d82b
shafikshaon <shafikshaon@gmail.com>
2024-05-19 20:30:18 +0600

    Register `TransactionCategory` for Admin Panel

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from wallets.models import Wallet
+from wallets.models import Wallet, TransactionCategory
 
 admin.site.register(Wallet)
+admin.site.register(TransactionCategory)
