bda89a7f5a411b560d3b30bc6cc0d326e51de9bf
shafikshaon <shafikshaon@gmail.com>
2024-05-19 22:45:40 +0600

    Add Dashboard View and Static Files, Templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from accounts.models import SystemUser
+
+
+class DashboardView(View):
+    template_name = "core/dashboard.html"
+    login_url = "/login/"
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            "total_active_user": SystemUser.objects.filter(is_active=True).count()
+        }
+        return render(request, self.template_name, context)
--- expense_note/urls.py
+++ expense_note/urls.py
@@ -5,8 +5,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import DashboardView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", DashboardView.as_view()),
 ]
 
 if config("DEBUG", cast=bool, default=True):
